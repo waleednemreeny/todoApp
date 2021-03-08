a7cd47934a5ca5639d07b58f603f4d6ff2724a37
waleednemreeny <[cinema4d.mkn@gmail.com]>
2021-03-08 13:28:48 +0200

    add oriject

---
+++ src/navigations/app-navigator.js
@@ -0,0 +1,23 @@
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import HomeScreen from '_scenes/home';
+
+
+const TabNavigatorConfig = {
+  initialRouteName: 'Home',
+  header: null,
+  headerMode: 'none',
+};
+
+const RouteConfigs = {
+  Home:{
+    screen:HomeScreen,
+  },
+  About:{
+    screen:AboutScreen,
+  },
+};
+
+const AppNavigator = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig);
+
+export default AppNavigator;
